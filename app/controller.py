1ef5d7ecba7d92cf6aa6d204001256bb84fc4b30
David Strain <strd79@hotmail.co.uk>
2020-09-18 18:07:11 +0100

    End of day commit from Friday

--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,17 @@
 from flask import render_template, request
 from app import app
 from app.models.player import Player
-from app.models.game import Game
+from app.models.game import *
 
 @app.route('/')
 def index():
-    return render_template("index.html", title="Game page")
+    return render_template("index.html", title="Player 1")
+
+@app.route("/player_1/<player_1_selection>", methods=["POST", "GET"])
+def player_2s_go(player_1_selection):
+    return render_template("index2.html/player_1_selection", title="Player 2")
+
+@app.route("/player_2/<player_1_selection>/<player_2_selection>", methods=["POST", "GET"])
+def the_winner_is(player_1_selction, player_2_selection):
+    who_wins(player_1_selction, player_2_selection)
+    return render_template("index3.html", title="The Winner!")
