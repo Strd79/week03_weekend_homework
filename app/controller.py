ea2c4a1c249890b8088c67264159ca4381b0682d
David Strain <strd79@hotmail.co.uk>
2020-09-18 10:57:44 +0100

    rest of files set-up

--- app/controller.py
+++ app/controller.py
@@ -1,3 +1,8 @@
 from flask import render_template, request
 from app import app
-from app.models.player import *
+from app.models.player import Player
+from app.models.game import Game
+
+@app.route('/')
+def index():
+    return render_template("index.html", title="Game page")
