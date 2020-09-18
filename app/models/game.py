9cf2d95afa913af036e4d03c9d757b1851f97d31
David Strain <strd79@hotmail.co.uk>
2020-09-18 11:39:07 +0100

    Completed 'who_wins' function in game.py

--- app/models/game.py
+++ app/models/game.py
@@ -1,2 +1,24 @@
+from player import Player
+
 class Game:
-    pass
+    
+    def __init__(self, input_player_1, input_player_2):
+        self.player_1 = input_player_1
+        self.player_2 = input_player_2
+
+    def who_wins(player_1, player_2):
+        if self.player_1.choice == "Rock" and self.player_2.choice == "Scissors":
+            return f"Player 1 WINS with {self.player_1.choice}!"
+        elif self.player_1.choice == "Rock" and self.player_2.choice == "Paper":
+            return f"Player 2 WINS with {self.player_2.choice}!"
+        elif self.player_1.choice == "Paper" and self.player_2.choice == "Rock":
+            return f"Player 1 WINS with {self.player_1.choice}!"
+        elif self.player_1.choice == "Paper" and self.player_2.choice == "Scissors":
+            return f"Player 2 WINS with {self.player_2.choice}!"
+        elif self.player_1.choice == "Scissors" and self.player_2.choice == "Paper":
+            return f"Player 1 WINS with {self.player_1.choice}!"
+        elif self.player_1.choice == "Scissors" and self.player_2.choice == "Rock":
+            return f"Player 2 WINS with {self.player_2.choice}!"
+        else:
+            return "It's a draw, play again!"
+        
--- app/models/player.py
+++ app/models/player.py
@@ -1,2 +1,5 @@
 class Player:
-    pass
+    
+    def __init__(self, input_name, input_choice):
+        self.name = input_name
+        self.choice = input_choice
